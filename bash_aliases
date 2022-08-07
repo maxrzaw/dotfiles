07ed816add98270e554bb853ec700bf1ebdde0d2
Max Zawisa <maxrzaw@umich.edu>
2022-08-07 17:10:56 -0400

    use nvim instead of vim

--- bash_aliases
+++ bash_aliases
@@ -1,2 +1,3 @@
 alias got='git'
 alias docker-ip="docker ps -q | xargs -n 1 docker inspect --format '{{ .Name }} {{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}}' | sed 's#^/##';"
+alias vim=nvim
--- install_symbolic_links.sh
+++ install_symbolic_links.sh
@@ -1,7 +1,8 @@
 # Creates symbolic links to the dotfiles repo from ~
 ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
+# ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
-mkdir -p ~/.vim/ftplugin
-ln -s ~/dotfiles/ftplugin/* ~/.vim/ftplugin
+# mkdir -p ~/.vim/ftplugin
+# ln -s ~/dotfiles/ftplugin/* ~/.vim/ftplugin
+ln -s ~/dotfiles/nvim/config/nvim ~/.config/nvim
