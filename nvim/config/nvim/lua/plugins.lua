b013826d575b224d029fe04055c4050aea7887e2
Max Zawisa <maxrzaw@umich.edu>
2022-08-07 21:21:18 -0400

    Add neogit

--- nvim/config/nvim/lua/plugins.lua
+++ nvim/config/nvim/lua/plugins.lua
@@ -20,6 +20,9 @@ return require('packer').startup(function(use)
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
+    -- Neogit
+    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
   if packer_bootstrap then
