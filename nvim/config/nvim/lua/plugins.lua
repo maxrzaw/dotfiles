06f8d85b7f45317c59ac00fea9803af681a6c088
Max Zawisa <maxrzaw@umich.edu>
2022-08-09 01:03:59 -0400

    Add telescope and some dependencies
    I have not configured it yet

--- nvim/config/nvim/lua/plugins.lua
+++ nvim/config/nvim/lua/plugins.lua
@@ -1,8 +1,8 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
 end
 
 return require('packer').startup(function(use)
@@ -20,12 +20,24 @@ return require('packer').startup(function(use)
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
+    -- Treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.x',
+        requires = {{ 'nvim-lua/plenary.nvim'}}
+    }
+
     -- Neogit
     use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
