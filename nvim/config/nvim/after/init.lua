0cbbe0237025b79dc41d1d69c7c646478e1db043
Max Zawisa <maxrzaw@umich.edu>
2022-08-09 20:26:05 -0400

    configure treesitter and telescope

--- nvim/config/nvim/after/init.lua
+++ nvim/config/nvim/after/init.lua
@@ -1,3 +1,14 @@
 local neogit = require('neogit')
 
 neogit.setup {}
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "maintained", -- Only use parsers that are maintained
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+    },
+}
+
