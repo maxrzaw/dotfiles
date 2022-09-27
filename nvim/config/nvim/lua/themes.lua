3b44fd6107f8d3f372e742c6b179a6f5c1ff27b0
Max Zawisa <maxrzaw@umich.edu>
2022-09-27 19:34:15 -0400

    add dark background to vim...

--- nvim/config/nvim/lua/themes.lua
+++ nvim/config/nvim/lua/themes.lua
@@ -6,5 +6,7 @@ vim.g.tokyonight_sidebars = { "terminal", "packer", "qf", "vista_kind" }
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
+vim.opt.background = "dark"
+
 vim.cmd [[colorscheme tokyonight]]
 vim.cmd[[highlight ColorColumn guibg='DarkRed']]
