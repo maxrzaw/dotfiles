ca1ec090793b67cec1f21845b1ce8b3bb5faebe9
Max Zawisa <maxrzaw@umich.edu>
2022-09-29 13:57:03 -0400

    reorganize

--- nvim/config/nvim/lua/options.lua
+++ nvim/config/nvim/lua/options.lua
@@ -6,11 +6,14 @@ vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
 
 -- Folding
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 3
 vim.opt.foldminlines=2
+--vim.opt.foldmethod = "expr"
 
 -- Search Options
 vim.opt.hlsearch = false -- This highlights search results.
@@ -19,37 +22,39 @@ vim.opt.incsearch = true -- Show incremental searches.
 vim.opt.smartcase = true -- Switch to case sensitive when uppercase is present in search.
 
 -- Interface options
--- Color Column
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.colorcolumn = {80,120}
 
 -- Line Wrapping
 vim.opt.textwidth = 80
--- Prevent automatic wrapping
-vim.opt.wrap = false
+vim.opt.wrap = false -- Prevent automatic wrapping
+
+-- Scrolloff
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
 -- Miscellaneous
 vim.opt.errorbells = false
 vim.opt.termguicolors = true
 vim.opt.mouse = "a"
 vim.opt.showmatch = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.history = 1000
 vim.opt.cmdheight = 1
 vim.opt.wildmenu = true
 vim.opt.autoread = true
 vim.opt.showcmd = true
-vim.opt.backupdir = vim.fn.stdpath('config') .. '/backup'
-vim.opt.directory = vim.fn.stdpath('config') .. '/swp'
+vim.opt.signcolumn = "yes"
 
--- COC
+-- Backup, history, and undo
 vim.opt.backup = true
 vim.opt.writebackup = true
+vim.opt.backupdir = vim.fn.stdpath('config') .. '/backup'
+vim.opt.directory = vim.fn.stdpath('config') .. '/swp'
+vim.opt.history = 1000
+vim.opt.undofile = true
+
 vim.opt.updatetime = 300
 vim.opt.timeoutlen = 500 -- how long to wait between key combinations
-vim.opt.signcolumn = "yes"
---vim.g.coc_node_path = '/c/Program Files/nodejs/node'
---vim.opt.foldmethod = "expr"
 
 -- netrw
 vim.g.netrw_bufsettings = "noma nomod nonu nobl nowrap ro rnu"
@@ -61,4 +66,5 @@ vim.g.netrw_winsize = 40
 if vim.fn.has("win32") then
     vim.opt.shell = 'bash.exe'
     vim.opt.shellcmdflag='-c'
+    --vim.g.coc_node_path = '/c/Program Files/nodejs/node'
 end
