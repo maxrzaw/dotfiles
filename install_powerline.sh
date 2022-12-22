e9ca0cc3808aaff92c6359feca939a5d9efdb38c
Max Zawisa <maxrzaw@umich.edu>
2022-12-21 22:37:02 -0500

    remove script

--- install_powerline.sh
+++
@@ -1,8 +0,0 @@
-# Install Powerline
-# Found some configuration tips here:
-# https://dane-bulat.medium.com/powerline-on-linux-an-integration-guide-c097831106f6
-pip install --user powerline-status
-# Install Powerline-gitstatus
-# Config instructions here:
-# https://github.com/jaspernbrouwer/powerline-gitstatus
-pip install powerline-gitstatus
