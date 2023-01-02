f5522054e8eff0209cb55c41653fea3627b5e5c6
Max Zawisa <maxrzaw@umich.edu>
2023-01-01 19:12:28 -0500

    add markdown-preview

--- nvim/config/nvim/lua/mzawisa/packer.lua
+++ nvim/config/nvim/lua/mzawisa/packer.lua
@@ -78,6 +78,13 @@ return require('packer').startup(function(use)
 
     use 'gpanders/editorconfig.nvim'
 
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+        ft = { "markdown" },
+    })
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
