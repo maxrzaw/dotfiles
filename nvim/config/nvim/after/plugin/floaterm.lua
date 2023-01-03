2ed20f200f9fdac1fd783d2d882c8876fca9e787
Max Zawisa <maxrzaw@umich.edu>
2023-01-03 14:08:31 -0500

    update floaterm command

--- nvim/config/nvim/after/plugin/floaterm.lua
+++ nvim/config/nvim/after/plugin/floaterm.lua
@@ -2,5 +2,7 @@ local nnoremap = require("mzawisa.keymap").nnoremap;
 
 vim.g.floaterm_title = "";
 -- Terminal Mode
-vim.keymap.set("t", "<ESC>", "<cmd>FloatermToggle<cr>", { noremap = true, silent = true });
-nnoremap("<leader>t", "<cmd>FloatermToggle<cr>", {});
+vim.keymap.set("t", "<C-T>", "<cmd>FloatermToggle<cr>", { noremap = true, silent = true });
+nnoremap("<C-T>", "<cmd>FloatermToggle<cr>", {});
+vim.g.floaterm_width = 0.9;
+vim.g.floaterm_height = 0.9;
