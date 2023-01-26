4ae1050251e59c10d4d32831c01b9ba17993f841
Max Zawisa <maxrzaw@umich.edu>
2023-01-26 14:38:13 -0500

    add mapping functions

--- nvim/config/nvim/lua/mzawisa/keymap.lua
+++ nvim/config/nvim/lua/mzawisa/keymap.lua
@@ -9,6 +9,8 @@ local function bind(op, outer_opts)
 end
 
 M.nmap = bind("n", { noremap = false });
+M.xmap = bind("x", { noremap = false });
+M.omap = bind("o", { noremap = false });
 M.nnoremap = bind("n");
 M.vnoremap = bind("v");
 M.xnoremap = bind("x");
