ce3bf12770888c40993db04847b79b5a9cf5b616
Max Zawisa <maxrzaw@umich.edu>
2023-06-29 21:28:29 -0400

    add git plugins

---
+++ nvim/config/nvim/after/plugin/git.lua
@@ -0,0 +1,10 @@
+local neogit = require('neogit');
+local nnoremap = require('mzawisa.keymap').nnoremap;
+
+neogit.setup {}
+
+nnoremap("<leader>gb", "<cmd>Git blame<cr>", {});
+
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    callback = function() require('lazygit.utils').project_root_dir() end
+})
