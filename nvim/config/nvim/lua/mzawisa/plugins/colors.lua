56414afc7e76e96587bf3071aae7e9c9c51eb38c
Max Zawisa <maxrzaw@umich.edu>
2023-07-03 00:05:13 -0400

    working on migrating to lazy.nvim

---
+++ nvim/config/nvim/lua/mzawisa/plugins/colors.lua
@@ -0,0 +1,13 @@
+function ColorMyPencils(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "DarkRed" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+end
+
+ColorMyPencils()
