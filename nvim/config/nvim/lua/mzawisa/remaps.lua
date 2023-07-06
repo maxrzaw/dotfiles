ea8f73a5bc22bebfdb3e78ac0c2a32c65e407907
Max Zawisa <maxrzaw@umich.edu>
2023-07-06 09:29:36 -0400

    adding in vscode checks

--- nvim/config/nvim/lua/mzawisa/remaps.lua
+++ nvim/config/nvim/lua/mzawisa/remaps.lua
@@ -4,29 +4,10 @@ local vnoremap = keymap.vnoremap
 local xnoremap = keymap.xnoremap
 local inoremap = keymap.inoremap
 local nmap = keymap.nmap
+local xmap = keymap.xmap
+local omap = keymap.omap
 
 vim.g.mapleader = " "
-
-nnoremap("<leader>e", "<cmd>Explore<CR>", {})
-vnoremap("J", ":m '>+1<CR>gv=gv", {})
-vnoremap("K", ":m '<-2<CR>gv=gv", {})
---
--- Map ; act like :
-vim.keymap.set("", ";", ":")
-
--- Map jk to <ESC> when in insert mode
-inoremap("jk", "<ESC>", {})
-
--- Keep me centered
-nnoremap("<C-u>", "<C-u>zz", {})
-nnoremap("<C-d>", "<C-d>zz", {})
-nnoremap("n", "nzzzv", {})
-nnoremap("N", "Nzzzv", {})
-vnoremap("<C-d>", "<C-d>zz", {})
-vnoremap("<C-u>", "<C-u>zz", {})
-
--- Folding
-nnoremap("<leader>z", "za", {})
 
 -- Yank the rest of a line
 nnoremap("Y", "yg$", {})
@@ -38,6 +19,9 @@ xnoremap("<leader>p", '"_dP', {})
 nnoremap("<leader>d", '"_d', {})
 vnoremap("<leader>d", '"_d', {})
 
+-- Map ; act like :
+vim.keymap.set("", ";", ":")
+
 -- Yank to system clipboard
 nnoremap("<leader>y", '"+y', {})
 vnoremap("<leader>y", '"+y', {})
@@ -45,3 +29,66 @@ nmap("<leader>Y", '"+Y', { silent = true }) -- I want this to remap
 
 nnoremap("<leader>lg", "<cmd>LazyGit<cr>", {})
 nnoremap("<leader>gb", "<cmd>Git blame<cr>", {})
+
+-- vscode-neovim specific mappings
+if vim.g.vscode then
+    -- Commenting
+    xmap("gc", "<Plug>VSCodeCommentary", {})
+    nmap("gc", "<Plug>VSCodeCommentary", {})
+    omap("gc", "<Plug>VSCodeCommentary", {})
+    nmap("gcc", "<Plug>VSCodeCommentaryLine", {})
+
+    -- Navigation
+    nnoremap("gr", "<cmd>call VSCodeNotify('editor.action.goToReferences')<CR>")
+    nnoremap("gd", "<cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>")
+    nnoremap("gt", "<cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>")
+    nnoremap("gi", "<cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>")
+
+    -- Refactoring
+    nnoremap("<leader>r", "<cmd>call VSCodeNotify('editor.action.rename')<CR>", {})
+
+    -- Move lines. This works, but is a bit jarring visually
+    xnoremap("J", ":m '>+1<CR>gv", {})
+    xnoremap("K", ":m '<-2<CR>gv", {})
+
+    -- Folding
+    nnoremap("za", "<cmd>call VSCodeNotify('editor.toggleFold')<CR>")
+    nnoremap("zR", "<cmd>call VSCodeNotify('editor.unfoldAll')<CR>")
+    nnoremap("zM", "<cmd>call VSCodeNotify('editor.foldAll')<CR>")
+
+    -- Open explorer with <leader>e
+    nnoremap("<leader>e", "<cmd>call VSCodeNotify('workbench.view.explorer')<CR>", {})
+    nnoremap("<leader>ff", "<cmd>Ex<CR>", {})
+    nnoremap("<leader>fg", "<cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", {})
+
+    -- Make o use the correct indentation
+    nnoremap(
+        "o",
+        "o<cmd>call VSCodeNotifyRange('editor.action.reindentselectedlines', line('.'), line('.'), 1)<CR>",
+        {}
+    )
+    nnoremap(
+        "O",
+        "O<cmd>call VSCodeNotifyRange('editor.action.reindentselectedlines', line('.')-1, line('.')-1, 1)<CR>",
+        {}
+    )
+else
+    -- Remaps I do not want in vscode-neovim
+    nnoremap("<leader>e", "<cmd>Explore<CR>", {})
+    vnoremap("J", ":m '>+1<CR>gv=gv", {})
+    vnoremap("K", ":m '<-2<CR>gv=gv", {})
+
+    -- Map jk to <ESC> when in insert mode
+    inoremap("jk", "<ESC>", {})
+
+    -- Keep me centered
+    nnoremap("<C-u>", "<C-u>zz", {})
+    nnoremap("<C-d>", "<C-d>zz", {})
+    nnoremap("n", "nzzzv", {})
+    nnoremap("N", "Nzzzv", {})
+    vnoremap("<C-d>", "<C-d>zz", {})
+    vnoremap("<C-u>", "<C-u>zz", {})
+
+    -- Folding
+    nnoremap("<leader>z", "za", {})
+end
