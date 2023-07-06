83391de12a9bb8e8154b9594881767e17fe6f290
Max Zawisa <maxrzaw@umich.edu>
2023-07-06 09:29:36 -0400

    adding in vscode checks

--- nvim/config/nvim/lua/mzawisa/set.lua
+++ nvim/config/nvim/lua/mzawisa/set.lua
@@ -9,11 +9,13 @@ vim.opt.smarttab = true
 vim.opt.list = true -- enable the below listchars
 vim.opt.listchars = { tab = "▸ ", trail = "·" }
 
--- Folding
-vim.opt.foldmethod = "indent"
-vim.opt.foldlevelstart = 3
-vim.opt.foldminlines = 2
---vim.opt.foldmethod = "expr"
+if not vim.g.vscode then
+    -- Folding
+    vim.opt.foldmethod = "indent"
+    vim.opt.foldlevelstart = 3
+    vim.opt.foldminlines = 2
+    --vim.opt.foldmethod = "expr"
+end
 
 -- Search Options
 vim.opt.hlsearch = false -- This highlights search results.
@@ -21,29 +23,34 @@ vim.opt.ignorecase = true -- This ignores case when searching.
 vim.opt.incsearch = true -- Show incremental searches.
 vim.opt.smartcase = true -- Switch to case sensitive when uppercase is present in search.
 
--- Interface options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.colorcolumn = { 120 }
--- Relativenumber for current buffer only
-vim.cmd([[
+if not vim.g.vscode then
+    -- Interface options
+    vim.opt.number = true
+    vim.opt.relativenumber = true
+    vim.opt.colorcolumn = { 120 }
+    -- Relativenumber for current buffer only
+    vim.cmd([[
         autocmd BufLeave * : setlocal norelativenumber
         autocmd BufEnter * : setlocal relativenumber
 ]])
+end
 
 -- Line Wrapping
 vim.opt.textwidth = 80
 vim.opt.wrap = false -- Prevent automatic wrapping
 
--- Scrolloff
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+if not vim.g.vscode then
+    -- Scrolloff
+    vim.opt.scrolloff = 8
+    vim.opt.sidescrolloff = 8
+end
 
 -- Miscellaneous
 vim.opt.errorbells = false
 vim.opt.termguicolors = true
 vim.opt.mouse = "a"
 vim.opt.showmatch = true
+-- These might need to be off in vscode
 vim.opt.cmdheight = 1
 vim.opt.wildmenu = true
 vim.opt.autoread = true
