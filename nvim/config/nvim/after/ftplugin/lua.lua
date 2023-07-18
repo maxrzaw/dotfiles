e81138e9a17994475bb78e3e11d8a2e325e48be9
Max Zawisa <maxrzaw@umich.edu>
2023-07-17 23:17:38 -0400

    colorscheme and lua formatting

--- nvim/config/nvim/after/ftplugin/lua.lua
+++ nvim/config/nvim/after/ftplugin/lua.lua
@@ -1,7 +1,10 @@
 -- formatting
 vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("UserLuaFormatting", {}),
-    callback = function()
-        require("stylua-nvim").format_file()
+    desc = "Format lua on write using stylua",
+    group = vim.api.nvim_create_augroup("UserLuaFormatting", { clear = true }),
+    callback = function(opts)
+        if vim.bo[opts.buf].filetype == "lua" then
+            require("stylua-nvim").format_file({ error_display_strategy = "none" })
+        end
     end,
 })
