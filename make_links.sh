d52c793862d8d0b2e9f6690afdb59844b6e859da
Max Zawisa <maxrzaw@umich.edu>
2023-08-03 21:59:13 -0400

    comment out old links

--- make_links.sh
+++ make_links.sh
@@ -5,23 +5,23 @@ if [ ! -d ~/.config/nvim ]; then
     echo "Created symbolic link from ~/.config/nvim to ~/dotfiles/nvim/config/nvim"
 fi
 
-if [ ! -d ~/.config/powerline ]; then
-    ln -s ~/dotfiles/powerline ~/.config/powerline
-    echo "Created symbolic link from ~/.config/powerline to ~/dotfiles/powerline"
-fi
+# if [ ! -d ~/.config/powerline ]; then
+#     ln -s ~/dotfiles/powerline ~/.config/powerline
+#     echo "Created symbolic link from ~/.config/powerline to ~/dotfiles/powerline"
+# fi
 
-# Set up come includes
-touch ~/.vimrc
-if test $(grep -c "~/dotfiles/vimrc" ~/.vimrc) = 0; then
-    echo "Adding vimrc to ~/.vimrc"
-    echo "source ~/dotfiles/vimrc" >> ~/.vimrc
-fi
+# Set up some includes
+#touch ~/.vimrc
+#if test $(grep -c "~/dotfiles/vimrc" ~/.vimrc) = 0; then
+#    echo "Adding vimrc to ~/.vimrc"
+#    echo "source ~/dotfiles/vimrc" >> ~/.vimrc
+#fi
 
-touch ~/.bashrc
-if test $(grep -c "~/dotfiles/bashrc" ~/.bashrc) = 0; then
-    echo "Adding bashrc to ~/.bashrc"
-    echo -e "if [ -f ~/dotfiles/bashrc ]; then\n    . ~/dotfiles/bashrc\nfi" >> ~/.bashrc
-fi
+#touch ~/.bashrc
+#if test $(grep -c "~/dotfiles/bashrc" ~/.bashrc) = 0; then
+#    echo "Adding bashrc to ~/.bashrc"
+#    echo -e "if [ -f ~/dotfiles/bashrc ]; then\n    . ~/dotfiles/bashrc\nfi" >> ~/.bashrc
+#fi
 
 if test $(grep -c "/dotfiles/gitconfig" ~/.gitconfig) = 0; then
     echo "Adding gitconfig to ~/.gitconfig"
