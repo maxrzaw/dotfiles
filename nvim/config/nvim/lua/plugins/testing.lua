ed3f178219b86c91f8bdc4a1b8977fa87ef1b8b0
Max Zawisa <maxrzaw@umich.edu>
2023-09-28 21:27:28 -0400

    testing and debug improvements

---
+++ nvim/config/nvim/lua/plugins/testing.lua
@@ -0,0 +1,50 @@
+-- Not super happy with either of these, I was able to copy the dap config from jester though
+return {
+    {
+        "David-Kunz/jester",
+        opts = {
+            cmd = "npx jest -t '$result' -- $file",
+            dap = {
+                sourceMaps = true,
+            },
+        },
+        config = function(opts)
+            require("jester").setup(opts)
+            vim.api.nvim_create_user_command("JesterDebugFile", "lua require('jester').debug_file()", {})
+            vim.api.nvim_create_user_command("JesterDebugLast", "lua require('jester').debug_last()", {})
+            vim.api.nvim_create_user_command("JesterDebugNearest", "lua require('jester').debug()", {})
+            vim.api.nvim_create_user_command("JesterRunFile", "lua require('jester').run_file()", {})
+            vim.api.nvim_create_user_command("JesterRunNearest", "lua require('jester').run()", {})
+            vim.api.nvim_create_user_command("JesterRunLast", "lua require('jester').run_last()", {})
+        end,
+    },
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-neotest/neotest-jest",
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-jest")({
+                        jestCommand = "npx jest",
+                        jestConfigFile = "jest.config.ts",
+                        env = { CI = true },
+                        cwd = function(_)
+                            return vim.fn.getcwd()
+                        end,
+                    }),
+                },
+            })
+            vim.api.nvim_create_user_command("NeotestRunFile", "lua require('neotest').run.run(vim.fn.expand('%'))", {})
+            vim.api.nvim_create_user_command(
+                "NeotestDebugNearest",
+                "lua require('neotest').run.run({strategy = 'dap'})",
+                {}
+            )
+        end,
+    },
+}
