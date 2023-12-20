442c46b7a32a390f667e35b4f9ab0452d32a8aba
Max Zawisa <maxrzaw@umich.edu>
2023-12-19 19:32:12 -0500

    remove whitespace removal

--- nvim/config/nvim/init.lua
+++ nvim/config/nvim/init.lua
@@ -90,9 +90,9 @@ require("lazy").setup({
                 local null_ls = require("null-ls")
                 null_ls.setup({
                     sources = {
-                        -- null_ls.builtins.code_actions.refactoring,
+                        null_ls.builtins.code_actions.refactoring,
                         null_ls.builtins.formatting.prettier,
-                        null_ls.builtins.formatting.trim_whitespace,
+                        -- null_ls.builtins.formatting.trim_whitespace, -- Causing a problem for markdown
                         null_ls.builtins.diagnostics.commitlint,
                         null_ls.builtins.diagnostics.todo_comments,
                         null_ls.builtins.diagnostics.dotenv_linter,
