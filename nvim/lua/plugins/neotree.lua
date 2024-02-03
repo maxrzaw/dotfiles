c6220f54ba5bd184c30e3a9f14fa4d5c1c341854
Max Zawisa <maxrzaw@umich.edu>
2024-02-03 14:43:02 -0500

    feat(neotree): modifications to how pinned works

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,63 +7,74 @@ return {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
     },
-    opts = {
-        close_if_last_window = true,
-        popup_border_style = "rounded",
-        event_handlers = {
-            {
-                event = "file_opened",
-                handler = function(_)
-                    if not neotree_helper.pinned() then
-                        require("neo-tree.command").execute({ action = "close" })
-                    end
-                end,
-            },
-            {
-                event = "neo_tree_buffer_enter",
-                handler = function(_)
-                    vim.opt_local.relativenumber = true
-                end,
-            },
-        },
-        window = {
-            position = "left",
-        },
-        filesystem = {
-            bind_to_cwd = false,
-            filtered_items = {
-                visible = true,
-            },
-            hijack_netrw_behavior = "open_current",
-            components = {
-                harpoon_index = function(config, node, _)
-                    local harpoon_list = require("harpoon"):list()
-                    local path = node:get_id()
-
-                    for i, item in ipairs(harpoon_list.items) do
-                        local value = item.value
-
-                        if value == path then
-                            return {
-                                text = string.format("->%d", i),
-                                highlight = config.highlight or "NeoTreeDirectoryIcon",
-                            }
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            popup_border_style = "rounded",
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(_)
+                        vim.notify("File opened: " .. vim.inspect(neotree_helper.pinned()))
+                        if not neotree_helper.pinned() then
+                            require("neo-tree.command").execute({ action = "close" })
                         end
-                    end
-                    return {}
-                end,
-            },
-            renderers = {
-                file = {
-                    { "icon" },
-                    { "name", use_git_status_colors = true },
-                    { "harpoon_index" },
-                    { "diagnostics" },
-                    { "git_status", highlight = "NeoTreeDimText" },
+                    end,
+                },
+                {
+                    event = "neo_tree_buffer_enter",
+                    handler = function(_)
+                        vim.opt_local.relativenumber = true
+                    end,
+                },
+                {
+                    event = "neo_tree_window_after_close",
+                    handler = function(_)
+                        if neotree_helper.pinned() then
+                            neotree_helper.unpin()
+                        end
+                    end,
                 },
             },
-        },
-    },
+            window = {
+                position = "left",
+            },
+            filesystem = {
+                bind_to_cwd = false,
+                filtered_items = {
+                    visible = true,
+                },
+                hijack_netrw_behavior = "open_current",
+                components = {
+                    harpoon_index = function(config, node, _)
+                        local harpoon_list = require("harpoon"):list()
+                        local path = node:get_id()
+
+                        for i, item in ipairs(harpoon_list.items) do
+                            local value = item.value
+
+                            if value == path then
+                                return {
+                                    text = string.format("->%d", i),
+                                    highlight = config.highlight or "NeoTreeDirectoryIcon",
+                                }
+                            end
+                        end
+                        return {}
+                    end,
+                },
+                renderers = {
+                    file = {
+                        { "icon" },
+                        { "name", use_git_status_colors = true },
+                        { "harpoon_index" },
+                        { "diagnostics" },
+                        { "git_status", highlight = "NeoTreeDimText" },
+                    },
+                },
+            },
+        })
+    end,
     keys = {
         {
             "<leader>e",
@@ -79,15 +90,6 @@ return {
                 require("neo-tree.command").execute({ action = "focus", position = "left" })
             end,
             desc = "Neotree Open and Pin",
-        },
-        {
-            "q",
-            function()
-                neotree_helper.unpin()
-                require("neo-tree.command").execute({ action = "close" })
-            end,
-            ft = { "neo-tree" },
-            desc = "Neotree Close",
         },
     },
 }
