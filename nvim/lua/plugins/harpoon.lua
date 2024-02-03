8561a91e3ea5cf4d00d863027e9e03c386ca0575
Max Zawisa <maxrzaw@umich.edu>
2024-02-03 11:18:56 -0500

    feat(harpoon): move my branch into my config and use Primes branch

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,12 +3,17 @@ local utils = require("mzawisa.utils")
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dev = true,
+    dev = false,
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local Path = require("plenary.path")
+        local function normalize_path(buf_name, root)
+            return Path:new(buf_name):make_relative(root)
+        end
         local Harpoon = require("harpoon")
+        local Logger = require("harpoon.logger")
+        local Extensions = require("harpoon.extensions")
         local default_config = Harpoon.config.default
 
         Harpoon:setup({
@@ -16,9 +21,137 @@ return {
                 save_on_toggle = true,
                 sync_on_ui_close = true,
                 key = function()
-                    -- return vim.uv.cwd()
+                    -- return vim.uv.cwd() -- This is the default
                     return utils.find_project_root()
                 end,
+            },
+            default = {
+
+                --- select_with_nill allows for a list to call select even if the provided item is nil
+                select_with_nil = false,
+
+                encode = function(obj)
+                    return vim.json.encode(obj)
+                end,
+
+                decode = function(str)
+                    return vim.json.decode(str)
+                end,
+
+                display = function(list_item)
+                    return normalize_path(list_item.value, vim.loop.cwd())
+                end,
+
+                select = function(list_item, list, options)
+                    Logger:log("config_default#select", list_item, list.name, options)
+                    options = options or {}
+                    if list_item == nil then
+                        return
+                    end
+
+                    local bufnr = vim.fn.bufnr(list_item.value)
+                    local set_position = false
+                    if bufnr == -1 then
+                        set_position = true
+                        bufnr = vim.fn.bufnr(list_item.value, true)
+                    end
+                    if bufnr == nil then
+                        error("bufnr was actually nil and not -1")
+                        return
+                    end
+                    if not vim.api.nvim_buf_is_loaded(bufnr) then
+                        vim.fn.bufload(bufnr)
+                        vim.api.nvim_set_option_value("buflisted", true, {
+                            buf = bufnr,
+                        })
+                    end
+
+                    if options.vsplit then
+                        vim.cmd("vsplit")
+                    elseif options.split then
+                        vim.cmd("split")
+                    elseif options.tabedit then
+                        vim.cmd("tabedit")
+                    end
+
+                    vim.api.nvim_set_current_buf(bufnr)
+
+                    if set_position then
+                        vim.api.nvim_win_set_cursor(0, {
+                            list_item.context.row or 1,
+                            list_item.context.col or 0,
+                        })
+                    end
+
+                    Extensions.extensions:emit(Extensions.event_names.NAVIGATE, {
+                        buffer = bufnr,
+                    })
+                end,
+
+                equals = function(list_item_a, list_item_b)
+                    return list_item_a.value == list_item_b.value
+                end,
+
+                get_root_dir = function()
+                    return vim.loop.cwd()
+                end,
+
+                create_list_item = function(_, item)
+                    if item == nil then
+                        item = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+                    end
+
+                    if type(item) == "string" then
+                        local name = Path:new(item):absolute()
+                        local bufnr = vim.fn.bufnr(name, false)
+
+                        local pos = { 1, 0 }
+                        if bufnr ~= -1 then
+                            pos = vim.api.nvim_win_get_cursor(0)
+                        end
+                        item = {
+                            value = name,
+                            context = {
+                                row = pos[1],
+                                col = pos[2],
+                            },
+                        }
+                    end
+
+                    Logger:log("config_default#create_list_item", item)
+
+                    return item
+                end,
+
+                BufLeave = function(arg, list)
+                    local bufnr = arg.buf
+                    local bufname = vim.api.nvim_buf_get_name(bufnr)
+                    local item = nil
+                    for _, it in ipairs(list.items) do
+                        local value = it.value
+                        if value == bufname then
+                            item = it
+                            break
+                        end
+                    end
+                    if item then
+                        local pos = vim.api.nvim_win_get_cursor(0)
+
+                        Logger:log(
+                            "config_default#BufLeave updating position",
+                            bufnr,
+                            bufname,
+                            item,
+                            "to position",
+                            pos
+                        )
+
+                        item.context.row = pos[1]
+                        item.context.col = pos[2]
+                    end
+                end,
+
+                autocmds = { "BufLeave" },
             },
             relative = {
                 select = function(list_item, list, options)
