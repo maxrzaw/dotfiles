6c82e3eb8edb554986433a3b451d31d404301ffb
Max Zawisa <maxrzaw@umich.edu>
2024-04-06 16:06:54 -0400

    I got jest working again :)

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,14 +1,10 @@
--- debug.lua
-
 return {
-    -- NOTE: Yes, you can install new plugins here!
     "mfussenegger/nvim-dap",
-    -- NOTE: And you can specify dependencies as well
     dependencies = {
         -- Creates a beautiful debugger UI
         {
             "rcarriga/nvim-dap-ui",
-            dependencies = {"nvim-neotest/nvim-nio"}
+            dependencies = { "nvim-neotest/nvim-nio" },
         },
 
         -- Virtual Text
@@ -31,54 +27,25 @@ return {
             },
         },
 
-        -- Installs the debug adapters for you
-        "williamboman/mason.nvim",
-        "jay-babu/mason-nvim-dap.nvim",
-
         -- Add your own debuggers here
         {
             "mxsdev/nvim-dap-vscode-js",
             opts = {
-                -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-                debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter", -- Path to vscode-js-debug installation.
-                -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-                adapters = { "pwa-node", "pwa-chrome", "node-terminal" }, -- which adapters to register in nvim-dap
-                -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-                -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-                -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+                debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+                adapters = { "pwa-node" },
             },
-            -- Commenting out until I get this figured out I am using mason
-            -- dependencies = {
-            --     {
-            --         "microsoft/vscode-js-debug",
-            --         build = { "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" },
-            --     },
-            -- },
-        },
-        {
-            "microsoft/vscode-node-debug2",
-            build = { "npm install && NODE_OPTIONS=--no-experimental-fetch npm run build && git restore ." },
+            dependencies = {
+                {
+                    "microsoft/vscode-js-debug",
+                    version = "1.x",
+                    build = { "npm i && npm run compile vsDebugServerBundle && mv dist out" },
+                },
+            },
         },
     },
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
-
-        require("mason-nvim-dap").setup({
-            -- Makes a best effort to setup the various debuggers with
-            -- reasonable debug configurations
-            automatic_setup = true,
-
-            -- You can provide additional configuration to the handlers,
-            -- see mason-nvim-dap README for more information
-            handlers = {},
-
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
-            ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
-            },
-        })
 
         local pb = require("persistent-breakpoints.api")
 
@@ -151,67 +118,33 @@ return {
             },
         })
 
-        -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+        -- Showing the UI with keymap and when debugging starts
         vim.keymap.set("n", "<F6>", dapui.toggle, { desc = "Debug: Toggle UI" })
         dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-        dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-        dap.listeners.before.event_exited["dapui_config"] = dapui.close
-
-        dap.adapters.node2 = {
-            type = "executable",
-            command = "node",
-            args = { os.getenv("HOME") .. "/.local/share/nvim/lazy/vscode-node-debug2/out/src/nodeDebug.js" },
-        }
 
         -- Install language specific config
-        -- I wasn't able to get this working with pwa-node, but neotest-jest does some magic and gets pwa-node working
         dap.configurations.typescript = {
             {
-                name = "Debug Current Directory Tests",
-                type = "node2",
+                type = "pwa-node",
                 request = "launch",
-                cwd = vim.fn.getcwd(),
-                trace = true, -- include debugger info
-                runtimeArgs = {
-                    "--inspect-brk",
-                    "./node_modules/jest/bin/jest.js",
-                    "--",
-                    function()
-                        return vim.fn.expand("%:p:h")
-                    end,
-                },
-                args = { "--no-cache" },
-                sourceMaps = true,
-                protocol = "inspector",
-                skipFiles = { "<node_internals>/**/*.js" },
-                console = "integratedTerminal",
-                port = 9229,
-                disableOptimisticBPs = true,
-            },
-            {
-                name = "Debug Matching Name Tests",
-                type = "node2",
-                request = "launch",
-                cwd = vim.fn.getcwd(),
+                name = "Debug Jest Tests",
                 -- trace = true, -- include debugger info
+                runtimeExecutable = "node",
                 runtimeArgs = {
                     "--inspect-brk",
                     "./node_modules/jest/bin/jest.js",
-                    function()
-                        return '--testNamePattern="' .. vim.fn.input("Test Name Pattern: ") .. '"'
-                    end,
-                    "--",
-                    function()
-                        return vim.fn.expand("%:p:h")
-                    end,
+                    "--runInBand",
                 },
-                args = { "--no-cache" },
-                sourceMaps = true,
-                protocol = "inspector",
-                skipFiles = { "<node_internals>/**/*.js" },
+                rootPath = "${workspaceFolder}",
+                cwd = "${workspaceFolder}",
                 console = "integratedTerminal",
-                port = 9229,
-                disableOptimisticBPs = true,
+                internalConsoleOptions = "neverOpen",
+                sourceMaps = true,
+                resolveSourceMapLocations = {
+                    "${workspaceFolder}/**",
+                    "!**/node_modules/**",
+                },
+                skipFiles = { "${workspaceFolder}/node_modules/**" },
             },
         }
     end,
