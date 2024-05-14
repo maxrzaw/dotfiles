5c1a4eb41b4d4e13c00163be4889f15cc4097022
Max Zawisa <maxrzaw@umich.edu>
2024-05-14 00:21:28 -0400

    found a new font!

--- wezterm/mzawisa/wezterm.lua
+++ wezterm/mzawisa/wezterm.lua
@@ -4,7 +4,7 @@ local keys = require("mzawisa.keys")
 local workspaces = require("mzawisa.workspaces")
 local act = wezterm.action
 
-local status, private_workspaces = pcall(require,"private.workspaces")
+local status, private_workspaces = pcall(require, "private.workspaces")
 if not status then
     private_workspaces = {}
 end
@@ -15,8 +15,8 @@ workspaces.setup(private_workspaces)
 tabs.setup(config)
 
 config.default_prog = { "zsh" }
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_prog = {"C:/Users/MZawisa/AppData/Local/Programs/Git/bin/bash.exe", "--login",  "-i"}
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { "C:/Users/MZawisa/AppData/Local/Programs/Git/bin/bash.exe", "--login", "-i" }
     config.launch_menu = {
         {
             label = "Git Bash",
@@ -25,12 +25,12 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
         {
             label = "PowerShell",
             domain = { DomainName = "local" },
-            args = {"powershell.exe"},
+            args = { "powershell.exe" },
         },
         {
             label = "Command Prompt",
             domain = { DomainName = "local" },
-            args = {"cmd.exe"},
+            args = { "cmd.exe" },
         },
     }
 end
@@ -39,8 +39,28 @@ config.audible_bell = "Disabled"
 
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.85
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+
+config.font = wezterm.font_with_fallback({ "Monaspace Neon", "FiraCode Nerd Font Mono" })
+config.harfbuzz_features = {
+    "calt", -- Contextual Alternates (texture healing)
+    "clig", -- Contextual Ligatures
+    "liga",
+    "ss01",
+    "ss02",
+    "ss03",
+    "ss04",
+    "ss05", -- Functional Programming Ligatures (F#)
+    "ss06",
+    "ss07",
+    "ss08",
+    "ss09",
+}
 config.font_size = 14.0
+local TIME_limit = false
+if TIME_limit then
+    config.font_size = 12.0
+end
+config.adjust_window_size_when_changing_font_size = false
 
 keys.setup(config)
 
