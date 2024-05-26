2672ebb73a1471614cd99de2aa4bcfaae820615b
Max Zawisa <maxrzaw@umich.edu>
2024-05-26 15:25:46 -0400

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,7 +44,7 @@ require("lazy").setup({
         {
             "catppuccin/nvim",
             name = "Catppuccin",
-            lazy = false, -- make sure we load this during startup if it is your main colorscheme
+            lazy = false,    -- make sure we load this during startup if it is your main colorscheme
             priority = 1000, -- make sure to load this before all the other start plugins
             config = function()
                 require("catppuccin").setup({
