0487f2f3b32af28331630ff5f35393e120cf30a6
Max Zawisa <maxrzaw@umich.edu>
2025-01-07 22:27:01 -0500

    add ; key for the 4th tab

--- wezterm/mzawisa/keys.lua
+++ wezterm/mzawisa/keys.lua
@@ -17,39 +17,40 @@ M.setup = function(config)
         { key = "^", mods = "CTRL", action = act.DisableDefaultAssignment },
 
         -- Tab Navigation
-        { key = '!', mods = 'LEADER', action = act.ActivateTab(0) },
-        { key = '1', mods = 'LEADER|SHIFT', action = act.ActivateTab(0) },
-        { key = 'j', mods = 'LEADER', action = act.ActivateTab(0) },
+        { key = "!", mods = "LEADER", action = act.ActivateTab(0) },
+        { key = "1", mods = "LEADER|SHIFT", action = act.ActivateTab(0) },
+        { key = "j", mods = "LEADER", action = act.ActivateTab(0) },
 
-        { key = '@', mods = 'LEADER', action = act.ActivateTab(1) },
-        { key = '2', mods = 'LEADER|SHIFT', action = act.ActivateTab(1) },
-        { key = 'k', mods = 'LEADER', action = act.ActivateTab(1) },
+        { key = "@", mods = "LEADER", action = act.ActivateTab(1) },
+        { key = "2", mods = "LEADER|SHIFT", action = act.ActivateTab(1) },
+        { key = "k", mods = "LEADER", action = act.ActivateTab(1) },
 
-        { key = '#', mods = 'LEADER', action = act.ActivateTab(2) },
-        { key = '3', mods = 'LEADER|SHIFT', action = act.ActivateTab(2) },
-        { key = 'l', mods = 'LEADER', action = act.ActivateTab(2) },
+        { key = "#", mods = "LEADER", action = act.ActivateTab(2) },
+        { key = "3", mods = "LEADER|SHIFT", action = act.ActivateTab(2) },
+        { key = "l", mods = "LEADER", action = act.ActivateTab(2) },
 
-        { key = '$', mods = 'LEADER', action = act.ActivateTab(3) },
-        { key = '4', mods = 'LEADER|SHIFT', action = act.ActivateTab(3) },
+        { key = "$", mods = "LEADER", action = act.ActivateTab(3) },
+        { key = "4", mods = "LEADER|SHIFT", action = act.ActivateTab(3) },
+        { key = ";", mods = "LEADER", action = act.ActivateTab(3) },
 
-        { key = '%', mods = 'LEADER', action = act.ActivateTab(4) },
-        { key = '5', mods = 'LEADER|SHIFT', action = act.ActivateTab(4) },
+        { key = "%", mods = "LEADER", action = act.ActivateTab(4) },
+        { key = "5", mods = "LEADER|SHIFT", action = act.ActivateTab(4) },
 
-        { key = '^', mods = 'LEADER', action = act.ActivateTab(5) },
-        { key = '6', mods = 'LEADER|SHIFT', action = act.ActivateTab(5) },
+        { key = "^", mods = "LEADER", action = act.ActivateTab(5) },
+        { key = "6", mods = "LEADER|SHIFT", action = act.ActivateTab(5) },
 
-        { key = '&', mods = 'LEADER', action = act.ActivateTab(6) },
-        { key = '7', mods = 'LEADER|SHIFT', action = act.ActivateTab(6) },
+        { key = "&", mods = "LEADER", action = act.ActivateTab(6) },
+        { key = "7", mods = "LEADER|SHIFT", action = act.ActivateTab(6) },
 
-        { key = '*', mods = 'LEADER', action = act.ActivateTab(7) },
-        { key = '8', mods = 'LEADER|SHIFT', action = act.ActivateTab(7) },
+        { key = "*", mods = "LEADER", action = act.ActivateTab(7) },
+        { key = "8", mods = "LEADER|SHIFT", action = act.ActivateTab(7) },
 
-        { key = '(', mods = 'LEADER', action = act.ActivateTab(8) },
-        { key = '9', mods = 'LEADER|SHIFT', action = act.ActivateTab(8) },
+        { key = "(", mods = "LEADER", action = act.ActivateTab(8) },
+        { key = "9", mods = "LEADER|SHIFT", action = act.ActivateTab(8) },
 
         -- Workspace Navigation
-        { key = 'n', mods = 'LEADER', action = act.SwitchToWorkspace { name = "nova" } },
-        { key = 'd', mods = 'LEADER', action = act.SwitchToWorkspace { name = "default" } },
+        { key = "n", mods = "LEADER", action = act.SwitchToWorkspace({ name = "nova" }) },
+        { key = "d", mods = "LEADER", action = act.SwitchToWorkspace({ name = "default" }) },
     }
 end
 
