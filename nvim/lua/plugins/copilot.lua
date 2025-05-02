5fbc0f97300699fa209d3f87055dd5919e5c7de1
Max Zawisa <maxrzaw@umich.edu>
2025-05-02 15:13:32 -0400

    change copilot keys
    For some reason <C-CR> is not working on windows in wezterm. I couldn't figure out why :(

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,4 @@
+local get_opts = require("mzawisa.keymap").get_opts
 return {
     { "github/copilot.vim", enabled = false, cond = not vim.g.vscode },
     {
@@ -31,38 +32,32 @@ return {
         },
         event = "VeryLazy",
         build = "make tiktoken",
-        opts = {
-            question_header = "#  User",
-            answer_header = "#  AI",
-            error_header = "#  ERROR",
-            separator = "",
-            highlight_headers = false,
+        opts = function()
+            require("CopilotChat").setup({
+                question_header = "#  User",
+                answer_header = "#  AI",
+                error_header = "#  ERROR",
+                separator = "",
+                highlight_headers = false,
 
-            context = {
-                "buffers",
-            },
-
-            model = "gpt-4o",
-            agent = "copilot",
-
-            window = {
-                border = "rounded",
-            },
-            show_help = false,
-            show_folds = false,
-
-            selection = false,
-
-            chat_autocomplete = false,
-
-            mappings = {
-                submit_prompt = {
-                    normal = "<C-CR>",
-                    insert = "<C-CR>",
+                context = {
+                    "buffers",
                 },
-            },
 
-            system_prompt = ([[
+                model = "gpt-4o",
+                agent = "copilot",
+
+                window = {
+                    border = "rounded",
+                },
+                show_help = false,
+                show_folds = false,
+
+                selection = false,
+
+                chat_autocomplete = false,
+
+                system_prompt = ([[
                     You are an advanced expert code-focused AI programming assistant helping with advanced topics.
                     You answer search-like questions, help with refactoring, optimizations and ideas.
                     You answer with succinctness and clarity. You do not include unnecessary explanations, comments and notes until user asks for them.
@@ -70,12 +65,14 @@ return {
                     Your user is an expert programmer, using Python, Rust, Bash and Linux.
                 ]]):gsub("%s+", " "),
 
-            prompts = {
-                ask = "I ask general question, not related to current project. I want generic answer with details and explanation",
-                explain = "Explain what selected code is doing",
-                opt = "Optimize selected code, if there is no guaranteed straightforward solution, try to give advices on optimization or guide on where to reseach. If there is guaranteed straightforward optimizations, explain them too",
-                write = "Implement a new part of code I'll ask for. Make code complete, correct and clean like I would write myself. Write: ",
-            },
-        },
+                prompts = {
+                    ask = "I ask general question, not related to current project. I want generic answer with details and explanation",
+                    explain = "Explain what selected code is doing",
+                    opt = "Optimize selected code, if there is no guaranteed straightforward solution, try to give advices on optimization or guide on where to reseach. If there is guaranteed straightforward optimizations, explain them too",
+                    write = "Implement a new part of code I'll ask for. Make code complete, correct and clean like I would write myself. Write: ",
+                },
+            })
+            vim.keymap.set({ "n", "i" }, "<M-CR>", "<cmd>CopilotChatToggle<cr>", get_opts("Copilot Chat Open"))
+        end,
     },
 }
