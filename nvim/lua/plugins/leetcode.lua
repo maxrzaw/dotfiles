5300f5bf1f8b091ac5af72c62d878b560701d2bd
Max Zawisa <maxrzaw@umich.edu>
2026-05-12 00:01:50 -0400

    Add leetcode plugin

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,47 @@
+return {
+    {
+        "kawre/leetcode.nvim",
+        cmd = "Leet",
+        cond = not vim.g.vscode,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim",
+        },
+        keys = {
+            {
+                "<leader>lc",
+                "<cmd>Leet<cr>",
+                desc = "LeetCode",
+            },
+        },
+        opts = {
+            lang = "typescript",
+            picker = {
+                provider = "telescope",
+            },
+            plugins = {
+                non_standalone = true,
+            },
+            hooks = {
+                enter = {
+                    function()
+                        local ok, command = pcall(require, "copilot.command")
+                        if ok then
+                            command.disable()
+                        end
+                    end,
+                },
+                leave = {
+                    function()
+                        local ok, command = pcall(require, "copilot.command")
+                        if ok then
+                            command.enable()
+                        end
+                    end,
+                },
+            },
+        },
+    },
+}
